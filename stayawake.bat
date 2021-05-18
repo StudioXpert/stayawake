5eb3b73430a58f4a0c61ae9c385f2fdfaf32818f
StudioXpert <84304648+StudioXpert@users.noreply.github.com>
2021-05-18 09:48:22 +0700

    Update stayawake.bat
    Add donate info

--- stayawake.bat
+++ stayawake.bat
@@ -1,6 +1,13 @@
-echo start
+cls
+echo off
+cls
 :loop
 cls
+echo =================================================================
+echo Dev by Studio Xpert
+echo donate at https://tipme.in.th/sugaraddict
+echo           paypal 
+echo =================================================================
 wmic logicaldisk get name, volumename, FreeSpace
 @echo off
 timeout /t 10
